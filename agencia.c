59427986c40db1e75a6b173219bbbf156c36f9f3
edely <edely.gomes@gmail.com>
2018-09-11 07:07:24 -0300

    cria e libera fila

--- agencia.c
+++ agencia.c
@@ -2,19 +2,47 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct cliente{
+    int CPF;
+    char operacao[1];
+    int valor;
+    int CPF_terceiro;
+    struct cliente *proximo;
+};
+
+typedef struct cliente t_cliente;
+typedef struct cliente *Guiche;
+
+Guiche* criaFila(){
+    Guiche* G = malloc(sizeof(Guiche));
+    if(G != NULL){
+        *G = NULL;
+    }
+    return G;
+}
+
+void zeraFila(Guiche *G){
+
+    if( G != NULL){
+        t_cliente *no;
+        while((*G) != NULL){
+            no = *G;
+            *G = (*G)->proximo;
+            free(no);
+        }
+        free(G);
+    }
+    return 0;
+}
+
 int main(){
+    //criar uma lista circular com cabeca que acessa uma fila de structs
 
-    // tres guiches, n clientes
+    // tres guiches com n clientes
 
-    struct cliente{
-        int CPF;
-        char operacao[1];
-        int valor;
-        int CPF_terceiro;
-    };
+    //implementar Fila de clientes
 
-    typedef struct Cliente cli;
-    typedef struct Cliente Guiche;
+    //implementar uma Lista de guiches
 
     return 0;
 }
