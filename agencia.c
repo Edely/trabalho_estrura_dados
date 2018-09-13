a4ab990b9b2d4c4902eb37fc5c2ed509d6245b5e
edely <edely.gomes@gmail.com>
2018-09-13 10:49:21 -0300

    despair

--- agencia.c
+++ agencia.c
@@ -26,7 +26,6 @@ typedef struct guiche Guiche;
 typedef struct elemento Elem;
 typedef struct guiche Fila;
 
-
 Fila* cria_fila(){
     Fila* fi = (Fila*) malloc(sizeof(Fila));
     if(fi != NULL){
@@ -63,13 +62,41 @@ int consulta_fila(Fila *fi, struct cliente *cl){
         return 0;
     else 
         return no->dados.CPF;
-    
-
+        
     return 1;
 }
 
-int main(){
+int main(int argc, char *argv[])
+    {
     int N;
+    int count = 1;
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0; 
+    size_t read;
+    char * line_str;
+    fp = fopen (argv[1], "r");
+
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        printf("%i\n", count);
+        //printf("Retrieved line of length %zu :\n", read);
+        
+
+        if(count == 1){
+            printf("%s\n", line);
+        }
+        count++;
+    }
+
+    fclose(fp);
+    if (line)
+        free(line);
+    exit(EXIT_SUCCESS);
+    
+    //printf(fp);
     //criar uma lista circular com cabeca que acessa uma fila de structs
     // tres guiches com n clientes
 
@@ -79,3 +106,32 @@ int main(){
 
     return 0;
 }
+/*
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    size_t read;
+
+    fp = fopen("/etc/motd", "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        printf("Retrieved line of length %zu :\n", read);
+        printf("%s", line);
+    }
+
+    fclose(fp);
+    if (line)
+        free(line);
+    exit(EXIT_SUCCESS);
+}
+
+*/
