b76b6efef335d1ed75c885b37cedf7445d08c16b
edely <edely.gomes@gmail.com>
2018-09-12 22:58:43 -0300

    adiciona funcoes

---
+++ FilaDin.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+
+struct aluno{
+    int matricula;
+    char nome[30];
+    float n1, n2, n3;
+};
+
+struct elemento{
+    struct aluno dados;
+    struct elemento *prox;
+};
+
+struct fila{
+    struct elemento *inicio;
+    struct elemento *final;
+};
+
+typedef struct fila Fila;
+
+typedef struct elemento Elem;
+
+
+Fila* cria_fila(){
+    Fila* fi = (Fila*) malloc(sizeof(Fila));
+    if(fi != NULL){
+        fi->final = NULL;
+        fi->inicio = NULL;
+        return 0;
+    }
+    return fi;
+}
+
+void libera_fila(Fila *fi){
+    if(fi != NULL){
+        Elem *no;
+        while(fi->inicio !=NULL){
+            no = fi->inicio;
+            fi->inicio = fi->inicio->prox;
+            free(no);
+        }
+        free(fi);
+    }
+}
+
+int tamanho_fila(Fila *fi){
+    if(fi == NULL) return 0;
+    Elem *no = fi->inicio;
+    int count = 0;
+    while(no != NULL){
+        count++;
+        no = no->prox;
+    }
+    return count;
+}
+
+int fila_vazia(Fila *fi){
+    if(fi == NULL) return 1;
+    if(fi->inicio == NULL)
+        return 1;
+    return 0;
+}
+
+int insere_fila(Fila *fi, struct aluno al){
+    if(fi == NULL) return 0;
+    Elem *no = (Elem*) malloc(sizeof(Elem));
+    no->dados = al;
+    no->prox = NULL;
+    if(fi->final == NULL){
+        fi->inicio = no;
+    }else{
+        fi->final->prox = no;
+    }
+    fi->final = no;
+    return 1;
+}
+
+int remove_fila(Fila *fi){
+    if(fi == NULL) return 0;
+    if(fi->inicio == NULL)
+        return 0;
+    Elem *no = fi->inicio;
+    fi->inicio = fi->inicio->prox;
+    if(fi->inicio == NULL)
+        fi->final = NULL;                
+    free(no);
+    return 1;
+}
+
+
+int consulta_fila(Fila *fi, struct aluno *al){
+    if(fi == NULL) return 0;
+    if(fi->inicio == NULL)
+        return 0;
+    *al = fi->inicio->dados;
+    return 1;
+}
+int main(){
+
+    Fila *fi;
+
+    fi = cria_fila();
+
+    libera_fila(fi);
+
+    return 0;
+}
--- agencia.c
+++ agencia.c
@@ -10,34 +10,67 @@ struct cliente{
     struct cliente *proximo;
 };
 
-typedef struct cliente t_cliente;
-typedef struct cliente *Guiche;
+struct elemento{
+    struct cliente dados;
+    struct elemento *prox;
+};
 
-Guiche* criaFila(){
-    Guiche* G = malloc(sizeof(Guiche));
-    if(G != NULL){
-        *G = NULL;
+struct guiche{
+    int n_operacoes;
+    struct elemento *inicio;
+    struct elemento *final;
+};
+
+typedef struct cliente t_cliente;
+typedef struct guiche Guiche;
+typedef struct elemento Elem;
+typedef struct guiche Fila;
+
+
+Fila* cria_fila(){
+    Fila* fi = (Fila*) malloc(sizeof(Fila));
+    if(fi != NULL){
+        fi->final = NULL;
+        fi->inicio = NULL;
+        return 0;
     }
-    return G;
+    return fi;
 }
 
-void zeraFila(Guiche *G){
-
-    if( G != NULL){
-        t_cliente *no;
-        while((*G) != NULL){
-            no = *G;
-            *G = (*G)->proximo;
+void libera_fila(Fila *fi){
+    if(fi != NULL){
+        Elem *no;
+        while(fi->inicio !=NULL){
+            no = fi->inicio;
+            fi->inicio = fi->inicio->prox;
             free(no);
         }
-        free(G);
+        free(fi);
     }
-    return 0;
+}
+
+int consulta_fila(Fila *fi, struct cliente *cl){
+    if(fi == NULL) return 0;
+    if(fi->inicio == NULL)
+        return 0;
+    Elem *no = fi->inicio;
+    int posicao = 0;
+    while(no->dados.CPF != cl->CPF){
+        no = no->prox;
+        posicao++;    
+    }
+    if(no->prox == NULL) 
+        return 0;
+    else 
+        return no->dados.CPF;
+    
+
+    return 1;
 }
 
 int main(){
+    int N;
     //criar uma lista circular com cabeca que acessa uma fila de structs
-
     // tres guiches com n clientes
 
     //implementar Fila de clientes
