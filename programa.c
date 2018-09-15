5811b15a6fc7fb158c5cb5fc57ea690cd9123a0a
edely <edely.gomes@gmail.com>
2018-09-15 12:58:26 -0300

    inserindo funcoes de fila

--- programa.c
+++ programa.c
@@ -15,16 +15,76 @@ struct elemento{
     struct elemento *prox;
 };
 
+struct fila{
+    struct elemento *inicio;
+    struct elemento *final;
+};
+
+
+/*uma fila de clientes */
+typedef struct elemento* fila;
+typedef struct fila Fila;
+
 /*o guiche eh uma pilha*/
 typedef struct elemento* Guiche;
 
-
+/* type para cliente */
 typedef struct cliente t_cliente;
 
 /*elemento da pilha*/
 typedef struct elemento Elem;
 
-/*funcoes*/
+/* funcoes para fila */
+Fila* cria_fila(){
+    Fila* fi = (Fila*) malloc(sizeof(Fila));
+    if(fi != NULL){
+        fi->final = NULL;
+        fi->inicio = NULL;
+        return 0;
+    }
+    return fi;
+}
+
+void libera_fila(Fila *fi){
+    if(fi != NULL){
+        Elem *no;
+        while(fi->inicio !=NULL){
+            no = fi->inicio;
+            fi->inicio = fi->inicio->prox;
+            free(no);
+        }
+        free(fi);
+    }
+}
+
+int insere_fila(Fila *fi, struct cliente al){
+    Elem *no = (Elem*) malloc(sizeof(Elem));
+    if(fi == NULL) return 0;
+    no->dados = al;
+    no->prox = NULL;
+    if(fi->final == NULL){
+        fi->inicio = no;
+    }else{
+        fi->final->prox = no;
+    }
+    fi->final = no;
+    return 1;
+}
+
+int remove_fila(Fila *fi){
+    Elem *no = fi->inicio;
+    if(fi == NULL) return 0;
+    if(fi->inicio == NULL)
+        return 0;
+    
+    fi->inicio = fi->inicio->prox;
+    if(fi->inicio == NULL)
+        fi->final = NULL;                
+    free(no);
+    return 1;
+}
+
+/*funcoes para guiches*/
 Guiche* cria_guiche(){
     Guiche* pi = (Guiche*) malloc(sizeof(Guiche));
     if(pi != NULL)
@@ -44,6 +104,9 @@ void libera_guiche(Guiche *pi){
     }
 }
 
+
+
+
 int main(){
 
 	int CPF, CPFT, N, i, valor;
@@ -56,27 +119,28 @@ int main(){
 
 
     while(i<N){
-        t_cliente cl;
+        /* t_cliente cl;*/
+        
         scanf("%c", &temp);
         scanf("%i %i %c %i", &CPF, &CPFT, &operacao, &valor);
-
+        
+        /*
         
         cl.CPF = CPF;
         cl.CPF_terceiro = CPFT;
         cl.valor = valor;
         cl.operacao = operacao;
 
+
+        
         printf("Cliente nº: %i\n", i);
         printf("CPF nº %i\n", cl.CPF);
         printf("CPF de Terceiro nº %i\n", cl.CPF_terceiro);
         printf("Operacao do tipo %c:\n", cl.operacao);
         printf("Valor da operacao %i:\n", cl.valor);
-
         printf("\n");
-
-        i++;
-        /* printf("%d %d %c %d\n", CPF, CPFT, operacao, valor);*/
-  
+        */
+        i++;  
     } 
 	
 	return 0;
