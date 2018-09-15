f76da4861c6c86b6f1d3025695cd8a4c3b44685d
edely <edely.gomes@gmail.com>
2018-09-15 11:23:48 -0300

    testando primeiros passos do programa

---
+++ backup_programa.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//o cliente
+struct cliente{
+    int CPF;
+    char *operacao;
+    int valor;
+    int CPF_terceiro;
+};
+
+//no de cliente
+struct elemento{
+    struct cliente dados;
+    struct cliente *prox;
+};
+
+//o guiche eh uma fila
+struct guiche{
+    int n_operacoes;
+    struct cliente *inicio;
+    struct cliente *final;
+};
+
+typedef struct cliente t_cliente;
+typedef struct guiche t_guiche;
+
+typedef struct guiche Fila;
+
+
+Fila* cria_fila(){
+    Fila* fi = (Fila*) malloc(sizeof(Fila));
+    if(fi != NULL){
+        fi->final = NULL;
+        fi->inicio = NULL;
+        return 0;
+    }
+    return fi;
+}
+
+int main(int argc, char *argv[]){
+	int CPF, CPFT, N, i, valor;
+    char temp;
+    char *operacao;
+    i = 0;
+
+    //le numero de operacoes
+    scanf("%d", &N);
+
+    //criar lista circular pra guardar guiches
+
+    Fila *g1 = cria_fila();
+    Fila *g2 = cria_fila();
+    Fila *g3 = cria_fila();
+    /*
+    while(i<N){
+        scanf("%c", &temp);
+        scanf("%i %i %c %i", &CPF, &CPFT, &operacao, &valor);
+
+        t_cliente* cl;
+        cl->CPF = CPF;
+        cl->CPF_terceiro = CPFT;
+        cl->valor = valor;
+        cl->operacao = operacao;
+
+        printf("Cliente nº %i:\n", i);
+        printf("CPF nº %i:\n", cl->CPF);
+        printf("CPF de Terceiro nº %i:\n", cl->CPF_terceiro);
+        printf("Operacao do tipo %c:\n", cl->operacao);
+        printf("Valor da operacao %i:\n", cl->valor);
+
+        printf("\n");
+
+        i++;
+        //printf("%d %d %c %d\n", CPF, CPFT, operacao, valor);
+        
+    } 
+    */   
+	
+	return 0;
+}
--- programa.c
+++ programa.c
@@ -1,74 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+//o cliente
 struct cliente{
     int CPF;
     char *operacao;
     int valor;
     int CPF_terceiro;
-    struct cliente *proximo;
 };
 
-//o guiche eh uma fila
-struct guiche{
-    int n_operacoes;
-    struct cliente *inicio;
-    struct cliente *final;
+//no == cliente
+struct elemento{
+    struct cliente dados;
+    struct cliente *prox;
 };
 
-typedef struct cliente t_cliente;
-typedef struct guiche t_guiche;
+//o guiche eh uma pilha
+typedef struct elemento* Guiche;
 
-typedef struct guiche Fila;
+//elemento da pilha
+typedef struct elemento Elem;
 
+//funcoes
+Guiche* cria_guiche(){
+    Guiche* pi = (Guiche*) malloc(sizeof(Guiche));
+    if(pi != NULL)
+        *pi = NULL;
+    return pi;
+}
 
-Fila* cria_fila(){
-    Fila* fi = (Fila*) malloc(sizeof(Fila));
-    if(fi != NULL){
-        fi->final = NULL;
-        fi->inicio = NULL;
-        return 0;
+void libera_guiche(Guiche *pi){
+    if(pi != NULL){
+        Elem *no;
+        while((*pi) != NULL){
+            no = *pi;
+            *pi = (*pi)->prox;
+            free(no);
+        }
+        free(pi);
     }
-    return fi;
 }
 
 int main(int argc, char *argv[]){
-	int CPF, CPFT, N, i, valor;
-    char temp;
-    char *operacao;
-    i = 0;
 
-    //le numero de operacoes
-    scanf("%d", &N);
+    struct cliente *selina;
+    selina->CPF = 0;
+    selina->CPF_terceiro = 1;
+    selina->valor = 7;
+    selina->operacao = 'd';
 
-    //criar lista circular pra guardar guiches
-
-    Fila *g1 = cria_fila();
-    Fila *g2 = cria_fila();
-    Fila *g3 = cria_fila();
-    /*
-    while(i<N){
-        scanf("%c", &temp);
-        scanf("%i %i %c %i", &CPF, &CPFT, &operacao, &valor);
-
-        t_cliente* cl;
-        cl->CPF = CPF;
-        cl->CPF_terceiro = CPFT;
-        cl->valor = valor;
-        cl->operacao = operacao;
-
-        printf("Cliente nº %i:\n", i);
-        printf("CPF nº %i:\n", cl->CPF);
-        printf("CPF de Terceiro nº %i:\n", cl->CPF_terceiro);
-        printf("Operacao do tipo %c:\n", cl->operacao);
-        printf("Valor da operacao %i:\n", cl->valor);
-
-        printf("\n");
-
-        i++;
-        //printf("%d %d %c %d\n", CPF, CPFT, operacao, valor);
-        
-    } 
-    */   
+    
 	
 	return 0;
 }
